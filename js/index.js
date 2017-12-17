d37f784641deb49c9ee2f111180ab398fdfbe87a
dujunhui007 <810743626@qq.com>
2017-12-18 01:05:21 +0800

    add new html css js

--- js/index.js
+++ js/index.js
@@ -4,13 +4,33 @@ $(document).ready(
     // $('.deScroll').css('overflow', 'auto')
   }),
   $(window).scroll(function () {
+    var winHeight = $(this).height();
+    var winWidth = $(this).width();
+    var hrLine = $("#advantages hr");
+    var sTop = $(this).scrollTop();
+    var hrLineTop = $(hrLine).offset().top;
+    if ((sTop + winHeight) > (hrLineTop + 200)) {
+      $("#advantages hr").addClass("hrAnimate")
+    }
+
     if ($(window).scrollTop() > 0) {
-      $("nav").css("background", "#d3edf6");
+      $("nav").css("background", "#fff");
+      $("nav .navRightItems li a").css("color", "#000");
       $("nav .navRightItems ").addClass("moreThan").removeClass("equalThan");
     } else {
-      $("nav").css("background", "#28a5d0");
+      $("nav").css("background", "transparent");
+      $("nav .navRightItems li a").css("color", "#fff");
       $("nav .navRightItems ").addClass("equalThan").removeClass("moreThan");
       // $('.deScroll').css('overflow', 'hidden')
     }
+  }),
+
+  $("#downloads  .dlist").on("click", "li", function () {
+    // 设index为当前点击
+    var index = $(this).index();
+    // 点击添加样式利用siblings清除其他兄弟节点样式
+    $(this).addClass("active").siblings().removeClass("active");
+    // 同理显示与隐藏
+    $(this).parents(".downloadsContainerLeft").find(".dlsit-list li").eq(index).show().siblings().hide();
   })
 );
